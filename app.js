05f60172e39aa507e11bc338d66d2c623f3bf2c9
Jaehyun Chi <jaehchi@gmail.com>
2018-03-18 14:45:05 -0700

    [add] - finishing core game logic and adding colors to ease

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { question } = require('readline-sync');
+const chalk = require('chalk');
 
 class TicTacToe {
   constructor() {
@@ -12,30 +13,32 @@ class TicTacToe {
   }
   
   printBoard () {
-    console.log(`${this.board[0][0]} | ${this.board[0][1]} | ${this.board[0][2]}`);
-    console.log(`${this.board[1][0]} | ${this.board[1][1]} | ${this.board[1][2]}`);
-    console.log(`${this.board[2][0]} | ${this.board[2][1]} | ${this.board[2][2]}`);
+    console.log(chalk.black.bgWhite(` ${this.board[0][0]} | ${this.board[0][1]} | ${this.board[0][2]} `));
+    console.log(chalk.black.bgWhite(` ${this.board[1][0]} | ${this.board[1][1]} | ${this.board[1][2]} `));
+    console.log(chalk.black.bgWhite(` ${this.board[2][0]} | ${this.board[2][1]} | ${this.board[2][2]} `));
   }
 
   printInvalidMove( message ) {
-    console.log(`Invalid move: ${message}`);
+    console.log(chalk.red(`Invalid move: ${message}`));
   }
 
-
   printWinner () {
-    console.log(`Congratulations, ${this.player}! You won!`)
+    console.log(chalk.blue.bgWhite(`Congratulations, ${this.player}! You won!`));
   }
 
   printDraw () {
-    console.log(`Good game, well played, but this game is a draw!`)
+    console.log(chalk.yellow(`Good game, well played, but this game is a draw!`));
   }
 
   
   promptPlayerMove () {
     let move;
+
     do {
-      move = question('Make a move! ');
+      move = question(chalk.green(`${chalk.blue.underline.bold(`Player ${this.player},`)} It's your turn, make a move! `));
     } while ( this.isInvalidMove( move ) );
+
+    return this.convertMovetoRowCol( move );
   }
   
   isInvalidMove ( move ) {
@@ -44,20 +47,36 @@ class TicTacToe {
     if ( move < 1 || move > 9 ) {
       this.printInvalidMove('Please select a move between 1 - 9!');
       return true;
-    } else if ( isNaN(move) ) {
+    } else if ( isNaN( move ) ) {
       this.printInvalidMove('Please select a move that is an integer!');
       return true;
-    } 
-
-
+    } else if ( !this.isMoveAvailable( move )) {
+      this.printInvalidMove('Move taken... Please select another move!');
+      return true;
+    } else {
+      return false;
+    }
   }
   
   isMoveAvailable ( move ) {
-    //check to see if the move is available
+    const { row, col } = this.convertMovetoRowCol( move );
+    return typeof this.board[row][col] === 'number';
+  }
+
+  convertMovetoRowCol ( move ) {
+    const col = Math.floor( ( move - 1 ) % 3 );
+    const row = Math.floor( ( move - 1 ) / 3 );
+
+    return { row, col };
+  }
+
+  placeMove ( row, col ) {
+    this.board[row][col] = this.player;
+    this.moves++;
   }
 
   switchPlayers () {
-    this.player === 'X' ? this.player === 'O' : this.player === 'X';
+    this.player === 'X' ? this.player = 'O' : this.player = 'X';
   }
 
   //Game Logic
@@ -67,29 +86,46 @@ class TicTacToe {
   }
 
   isRowWinner ( row ) {
-    // checks each row if theres a winner
+    return this.areAllEqual( this.board[row][0], this.board[row][1], this.board[row][2] )
   }
 
   isColWinner ( col ) {
-    // check each col if theres a winner
+    return this.areAllEqual( this.board[0][col], this.board[1][col], this.board[2][col] )
   }
 
   isDiagonalWinner () {
-    // check both diagonals if theres a winner
+    return this.areAllEqual( this.board[0][0], this.board[1][1], this.board[2][2] ) || this.areAllEqual( this.board[0][2], this.board[1][1], this.board[2][0] )
   }
 
-  isWinner () {
-    // 
+  isWinner (row, col) {
+    return this.isRowWinner( row ) || this.isColWinner( col ) || this.isDiagonalWinner();
   }
 
   isDraw () {
     return this.moves === 9;
   }
 
+  play () {
+    this.printBoard();
+
+    if ( this.moves < 9 ) {
+      const { row, col } = this.promptPlayerMove();
+      this.placeMove( row, col );
+
+      if ( this.isWinner( row, col ) ) {
+        this.printBoard();
+        this.printWinner();
+      } else if ( this.isDraw() ) {
+        this.printDraw();
+      } else {
+        this.switchPlayers();
+        this.play();
+      }
+    }
+  }
+
 }
 
 const game = new TicTacToe();
-game.printBoard();
-game.promptPlayerMove();
-game.printBoard();
 
+game.play();
